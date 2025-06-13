68e0cf75f6c605598dfffaa49a2aefada2c40de8
pavilion <jakisfajnyktos@gmail.com>
2025-06-13 09:12:54 +0200

    model

---
+++ backend/backend/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Contact
+    {
+        [Required]
+        [MaxLength(15)]
+        public string name { get; set; }
+        [MaxLength(9)]
+        public string number { get; set; }
+
+    }
+}
